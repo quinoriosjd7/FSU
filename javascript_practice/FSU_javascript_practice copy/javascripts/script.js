dc2f62ce362cbc3b47f1829990695d2441933cd5
quinoriosJD7 <quino.rios.jd@hotmail.com>
2013-04-14 20:07:31 -0400

    screencast follow-ups
    follow alog on screencasts

--- javascript_practice/FSU_javascript_practice copy/javascripts/script.js
+++ javascript_practice/FSU_javascript_practice copy/javascripts/script.js
@@ -1,1 +1,15 @@
 document.querySelector("body").style.backgroundColor="#00FF00";
+var age = 24;
+var isStudent = False;
+var firstName = "Jorge";
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3//
+var number = 1;
+muppetNames[1] = "Rizzo";
+muppetNames[2] = "Fozzy";
+console.log(muppetNames[muppetNumber]); 
+var yearBorn = 1988;
+var age = 2012 - yearBorn;
+console.log(age);
+
+
+
