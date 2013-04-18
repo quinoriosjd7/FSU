e0095e238ce6ed620f232e9eccc1e329cf48889f
quinoriosJD7 <quino.rios.jd@hotmail.com>
2013-04-18 15:04:01 -0400

    Expressions worksheet
    Sparky the pitbull's age

---
+++ Expressions_Worksheet/FSU_javascript_practice/javascripts/script.js
@@ -0,0 +1,18 @@
+//Jorge Quinones Rios, April 17 2013, Expression worksheet//
+
+//Sparky the pitbull's age//
+
+//Sparky's age in human years//
+var humanAge = 1;
+
+//Sparky's age in doggy years//
+var dogYears = 7;
+
+//Sparky's age//
+var age = humanAge * dogYears;
+console.log(age);
+
+
+
+
+
