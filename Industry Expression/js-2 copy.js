17292ad98ef90f465404ad0afe2affd75815c72c
quinoriosJD7 <quino.rios.jd@hotmail.com>
2013-04-18 17:55:09 -0400

    Assignment: Expressions
    Industry Expression

---
+++ Industry Expression/js-2 copy.js
@@ -0,0 +1,13 @@
+//Jorge Quinones Rios, April 18 2013, Expressions
+
+//How many burgers I wrap in shift//
+
+//hours in a shift//
+var hours = 8;
+
+//how many burgers in an hour//
+var burgersPerhour = 70;
+
+//how many burgers in a shift//
+var burgersPershift = hours * burgersPerhour;
+console.log(burgersPershift);
