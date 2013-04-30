6fcde4e176cd50dc254554ef18092d7d686556a5
quinoriosJD7 <quino.rios.jd@hotmail.com>
2013-04-30 16:06:37 -0400

    wacky conditionals
    wacky conditionals

---
+++ week3_conditionals/wacky_conditionals/javascript/wacky.js
@@ -0,0 +1,18 @@
+//clown shoes worn in a week
+var clownShoesInaweek = 0;
+//clown shoes worn in a month
+var clownShosInamonth = 1;
+//clown shoes in a year
+var clownShosInayear = clownShosInamonth * 12;
+//toe infections in a year
+var toeInfections = clownShosInayear - 1;
+
+var clownsToeInfections = prompt("How many toe infections for a clown in a year?");
+if(clownsToeInfections >= 11) {
+    alert("Yes!");
+    console.log("Yes!");
+    
+    }else{
+        alert("Nope!");
+        console.log("Nope!");
+}
