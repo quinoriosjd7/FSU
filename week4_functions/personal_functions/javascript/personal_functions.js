ccd7cfeb4dc6a67eefa21d812d221d5a1335244e
quinoriosJD7 <quino.rios.jd@hotmail.com>
2013-05-01 20:50:35 -0400

    rough draft of personal functions
    might need alot of help and editing

--- week4_functions/personal_functions/javascript/personal_functions.js
+++ week4_functions/personal_functions/javascript/personal_functions.js
@@ -1,16 +1,19 @@
-//boy's age
-var age = 10;
-//minimum age to get on carnival ride
-var minimumAge = 13;
-//maximum age to get on carnival ride
+//boy's height
+var height = 7;
+//minimum height to get on boat ride
+var minimumHeight = 5;
+//maximum age to get on boat ride
 var maximumAge = 60;
-//if client is this old he may purchase an angus burger
-var permission = minimumAge;
+//if client is this old he may get on ride
+var permission = minimumHeight * 1;
 
 var howOld = prompt("Old enough to get on?");
-if(howOld >= 13 || howOld == 60)
+if(howOld >= 5 && howOld ==60)
         {
             alert("Enjoy!");
             console.log("Enjoy");
 }else{alert("Sorry, can't ride!");
 }
+function granted(){
+    (minimumHeight * 1)
+    console.log(minimumHeight);}
