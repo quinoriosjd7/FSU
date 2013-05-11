5e6b2dfef93c1d9574d9d98a3fc30a957d0f7635
quinoriosJD7 <quino.rios.jd@hotmail.com>
2013-05-11 15:20:33 -0400

    Expressions Worksheet Week1 Sec2
    changes

--- Expression_Worksheet/javascript/script.js
+++ Expression_Worksheet/javascript/script.js
@@ -9,22 +9,23 @@ var humanAge = 1;
 var dogYears = 7;
 
 //Sparky's age//
-var input = prompt("Enter Sparky's age")
-if(input <=6)
+var inputOne = prompt("Enter Sparky's age");
+var inputTwo = prompt("Enter your age");
+if(inputOne <=6)
 {
 alert("wrong!");    
 }
-if(input ==7)
+if(inputOne ==7)
 {
 alert("correct!");    
 }
-if(input >=8)
+if(inputOne >=8)
 {
 alert("wrong");    
 }
-var age = humanAge * dogYears;
+var age = parseInt * dogYears;
 alert("Sparky the pitbull's age is " + age);
-console.log("Sparky the pitbull's age" + age);
+console.log("Sparky the pitbull's age is " + age);
 
 
 
