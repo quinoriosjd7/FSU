e97ac1b77552a28397d69e972e11742c6d28525c
quinoriosJD7 <quino.rios.jd@hotmail.com>
2013-05-15 16:02:05 -0400

    Expressions_Personal
    changes

---
+++ Personal Expressions/javascript/js-1 copy.js
@@ -0,0 +1,57 @@
+//Jorge Quinones Rios, April 18 2013, Expressions & Conditions
+
+//How many times I eat meals in a week//
+var mealsAday = prompt("How many times do I eat in a week?");
+//if user types in number more than 21, "wrong" will appear then the option to retry the question
+if (mealsAday >21 || mealsAday <21)
+{
+    console.log("Wrong!");
+    alert("Wrong!");
+    var mealsAday = prompt("How many times do I eat in a week?");
+}
+
+do{
+    alert("Please enter a number");
+    console.log("Pleas enter a number");
+    var mealsAday = prompt("How many times do I eat in a week?")
+}while (isNaN(mealsAday));
+
+do{
+    alert("Wrong!");
+    console.log("Wrong!");
+    var mealsAday = prompt("How many times do I eat in a week?")
+}while (mealsAday >21 || mealsAday <21);
+
+
+//if user types in 21, the answer will be true and access will be granted
+if (mealsAday == 21)
+{
+    console.log("Correct!, you may proceed");
+    alert("Correct!, you may proceed, welcome");    
+}
+
+do
+{ var mealsAday = prompt("How many times do I eat in a week?");
+}while (alert == "Wrong!" || alert == "Please enter a number");
+    
+
+//when access is granted, user will be welcomed
+if (alert =="Correct!, I eat 21 meals in a week, you may proceed, welcome");
+   (console.log =="Correct!, I eat 21 meals in a week, you may proceed, welcome");
+{
+index.html;
+}
+
+
+
+
+//meals I eat a day//
+var mealsAday = 3;
+
+//how many days in a week//
+var daysInaweek = 7;
+
+//how many meals I eat a week//
+var mealsInaweek = mealsAday * daysInaweek;
+console.log("Meals in a week:" + mealsInaweek);
+alert("Meals in a week:" + mealsInaweek);
--- Personal Expressions/javascript/js-1.js
+++ Personal Expressions/javascript/js-1.js
@@ -1,6 +1,73 @@
 //Jorge Quinones Rios, April 18 2013, Expressions
 
 //How many times I eat in a week//
+var mealsAday = prompt("How many times do I eat in a week?");
+if (mealsAday > 21)
+{
+    console.log("Wrong!");
+    alert("Wrong!");
+    var mealsAday = prompt("How many times do I eat in a week?");
+}
+
+if (mealsAday < 21)
+{
+    console.log("Wrong!");
+    alert("Wrong!");
+    var mealsAday = prompt("How many times do I eat in a week?");
+    
+}
+
+if (mealsAday == 21)
+{
+    console.log("Correct!, you may proceed");
+    alert("Correct!, you may proceed");
+    
+}
+
+if (alert =="Correct!, you may proceed")
+   console.log("Correct!, you may proceed")
+    
+{
+        alert("Welcome!");
+        console.log("Welcome");
+}
+
+if (alert =="Welcome!");
+   console.log("Welcome!");
+   
+{
+index.html;
+}
+       
+if (mealsAday == "I don't know")
+{
+    console.log("Please enter a number");
+    alert("Please enter a number");
+}
+
+if (alert == "Please enter a number")
+   (console.log == "Please enter a number")
+{
+    alert("Try again")
+    console.log("Try again");
+}    
+if (alert == "Try again")
+   (console.log == "Try again")
+{
+    
+    var mealsAday = prompt("How many times do I eat in a week")
+    console.log("How many times do I eat in a week");//
+}
+
+if (alert == "var mealsAday = prompt")
+   console.log("var mealsAday")
+   
+{
+    alert("Try again")
+    console.log("Try again");
+    
+   }
+
 
 //meals I eat a day//
 var mealsAday = 3;
@@ -10,4 +77,5 @@ var daysInaweek = 7;
 
 //how many meals I eat a week//
 var mealsInaweek = mealsAday * daysInaweek;
-console.log(mealsInaweek);
+console.log("Meals in a week:" + mealsInaweek);
+alert("Meals in a week:" + mealsInaweek);
