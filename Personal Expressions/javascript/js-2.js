3d182c74259579fc0c87588c518e728e46b972d6
quinoriosJD7 <quino.rios.jd@hotmail.com>
2013-05-16 20:35:50 -0400

    Personal Expressions sec02
    updates

---
+++ Personal Expressions/javascript/js-2.js
@@ -0,0 +1,14 @@
+//Jorge Quinones Rios, April 18 2013, Expressions
+
+//How many times I eat pop tarts in a week//
+
+//poptarts I eat a day//
+var popTartsAday = 3 + 20 - 20;
+
+//how many days in a week//
+var daysInaweek = 7 + 1 - 1;
+
+//how many pop tarts I eat a week//
+var popTartsInaweek = popTartsAday * daysInaweek + 0;
+console.log("I eat" + ' ' +popTartsInaweek+ ' ' + "PopTarts in a week");
+alert("I eat" + ' ' +popTartsInaweek+ ' ' + "PopTarts in a week");
--- Personal Expressions/javascript/scripts/js-1 copy.js
+++ Personal Expressions/javascript/scripts/js-1 copy.js
@@ -1,13 +1,21 @@
-//Jorge Quinones Rios, April 18 2013, Expressions & Conditions
+//Jorge Quinones Rios, April 18 2013, Expressions & 
 
 //How many times I eat meals in a week//
 var mealsAday = prompt("How many times do I eat in a week?");
+
 //if user types in a number more higher or lower than 21, "wrong" will appear then the option to retry the question
 if (mealsAday >21 || mealsAday <21)
 {
     console.log("Wrong!");
     alert("Wrong!");
     var mealsAday = prompt("How many times do I eat in a week?");
+}
+//if user types in 21, the answer will be true and access will be granted
+if (mealsAday == 21)
+{
+    console.log("Correct!, you may proceed");
+    alert("Correct!, you may proceed, welcome");
+    index.html;
 }
 //prompts user for a number entry while user enters a letter or word//
 do{
@@ -22,20 +30,6 @@ do{
     console.log("Wrong!");
     var mealsAday = prompt("How many times do I eat in a week?")
 }while (mealsAday >21 || mealsAday <21);
-
-
-//if user types in 21, the answer will be true and access will be granted
-if (mealsAday == 21)
-{
-    console.log("Correct!, you may proceed");
-    alert("Correct!, you may proceed, welcome");    
-}
-
-do
-{ var mealsAday = prompt("How many times do I eat in a week?");
-}while (alert == "Wrong!" || alert == "Please enter a number");
-    
-
 //when access is granted, user will be welcomed
 if (alert =="Correct!, I eat 21 meals in a week, you may proceed, welcome");
    (console.log =="Correct!, I eat 21 meals in a week, you may proceed, welcome");
@@ -47,12 +41,12 @@ index.html;
 
 
 //meals I eat a day//
-var mealsAday = 3;
+var mealsAday = 3 + 0;
 
 //how many days in a week//
-var daysInaweek = 7;
+var daysInaweek = 7 + 0;
 
 //how many meals I eat a week//
-var mealsInaweek = mealsAday * daysInaweek;
+var mealsInaweek = mealsAday * daysInaweek - 0;
 console.log("Meals in a week:" + mealsInaweek);
 alert("Meals in a week:" + mealsInaweek);
