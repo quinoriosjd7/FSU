c54a76a9bb94c1da3cfa527ae5afc07b26050a3d
quinoriosJD7 <quino.rios.jd@hotmail.com>
2013-05-17 19:32:36 -0400

    Expressions_Wacky
    updates

--- Expressions_Wacky/javascript/script.js
+++ Expressions_Wacky/javascript/script.js
@@ -1,7 +1,12 @@
 //Jorge Quinones Rios, May 16 2013, Expressions wacky//
 
+
+//current date//
+var date = new Date();
+
 //User guesses how many shoes a clown wears in a lifetime//
 var input = prompt("How many shoes does a clown wear in a lifetime?");
+
 
 
 //Clowns shoes worn in a lifetime//
@@ -18,8 +23,9 @@ var birthdaysPeryear = 84;
 //Total of clown shoes worn in lifetime//
 var clownsShoesPerlifetime = birthdaysPeryear * 50;
 alert("Clowns wear " + ' ' + clownsShoesPerlifetime + ' ' + "shoes in a lifetime");
+alert(date);
 console.log("Clowns wear " + ' ' + clownsShoesPerlifetime + ' ' + "shoes in a lifetime");
-
+console.log(date);
 
 
 
