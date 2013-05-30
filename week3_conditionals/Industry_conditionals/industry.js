f5a3ee91ee20a21a771e47bb4ac98febecce1df8
quinoriosJD7 <quino.rios.jd@hotmail.com>
2013-05-30 11:37:06 -0400

    Industry conditionals modifications
    file modification

--- week3_conditionals/Industry_conditionals/industry.js
+++ week3_conditionals/Industry_conditionals/industry.js
@@ -1,19 +1,19 @@
-//amount of websites due
-var totalWebsites = 5;
-//amount of reasonable websites to proceed with next projects
-var minWebsites = 3;
+//amount of art websites due//    May 23 2013
+var totalArtWebsites = 5;
+//amount of reasonable websites to proceed with future projects
+var minArtWebsites = 3;
 //maximum total of websites to receive bonus
-var maxWebsites = 5;
+var maxArtWebsites = 5;
 //if client receives this total of websites you will receive extra pay
-var extraPay = minWebsites+maxWebsites;
+var extraPay = minArtWebsites+maxArtWebsites;
 
-var WebsitesFinishedTypeIn = prompt("Websites completed");
+var WebsitesFinishedTypeIn = prompt("How many Websites completed");
 if(WebsitesFinishedTypeIn >= 8) {
     alert("Bonus!");
     var extraPay
     console.log("Bonus!");
-    
-    }else{
+    do{WebsitesFinishedTypeIn = prompt("How many Websites completed");continue;}while(isNaN(WebsitesFinishedTypeIn) == true);
+}else{
         alert("Sorry, no bonus for you!");
         console.log("Sorry, no bonus for you!");
 }
