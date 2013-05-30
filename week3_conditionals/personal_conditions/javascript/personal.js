327a52bb8d449ca421b5ed07c4ad68c0c703cf95
quinoriosJD7 <quino.rios.jd@hotmail.com>
2013-05-30 16:33:10 -0400

    Personal conditionals modification
    javascript modified

--- week3_conditionals/personal_conditions/javascript/personal.js
+++ week3_conditionals/personal_conditions/javascript/personal.js
@@ -5,15 +5,14 @@ var minimumAge = 12;
 //maximum age to eat an angus burger
 var maximumAge = 24;
 //if client is this old he may purchase an angus burger
-var angusBurger = minimumAge||maximumAge;
+var angusBurger = minimumAge;
 
 var clientAgeTypeIn = prompt("Old enough to eat an Angus burger?");
-if(clientAgeTypeIn >= 12 && clientAgeTypeIn <= 24){
-    alert("Enjoy!");
+if(kidAge == 13){
+    alert("Enjoy!")
     var angusBurger;
-    console.log("Enjoy!");
     
     }else{
-        alert("Sorry, you can't eat this!");
-        console.log("Sorry, you can't eat this");
+        alert("Sorry, you can't eat this!")
+    
 }
