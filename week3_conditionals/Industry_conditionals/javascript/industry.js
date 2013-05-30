c81fec36b18be09b0e8789b26da6c0fcf6a76da6
quinoriosJD7 <quino.rios.jd@hotmail.com>
2013-05-30 12:35:48 -0400

    Industry conditionals modified
    file modified

--- week3_conditionals/Industry_conditionals/javascript/industry.js
+++ week3_conditionals/Industry_conditionals/javascript/industry.js
@@ -1,19 +1,42 @@
-//amount of websites due
-var totalWebsites = 5;
-//amount of reasonable websites to proceed with next projects
-var minWebsites = 3;
-//maximum total of websites to receive bonus
-var maxWebsites = 5;
-//if client receives this total of websites you will receive extra pay
-var extraPay = minWebsites+maxWebsites;
+//amount of art websites due//    May 23 2013
+var totalArtWebsites = 5;
 
-var WebsitesFinishedTypeIn = prompt("Websites completed");
-if(WebsitesFinishedTypeIn >= 8) {
-    alert("Bonus!");
+//amount of reasonable websites to proceed with future projects
+var minArtWebsites = 3;
+
+//maximum total of websites to receive bonus
+var maxArtWebsites = 5;
+
+//if client receives this total of websites you will receive extra pay
+var extraPay = minArtWebsites+maxArtWebsites;
+
+//prompts employee to input number of Art websites developed//
+var WebsitesFinishedTypeIn = prompt("How many Websites completed");
+
+//if employee inputs any number ranging from 8 or more they will receive an alert congratulating them on a bonus received//
+if(WebsitesFinishedTypeIn ==8)
+{
+    alert("Congratulations you receive a bonus!");
     var extraPay
-    console.log("Bonus!");
+    console.log("Congratulations you receive a bonus!");
+    do{WebsitesFinishedTypeIn = prompt("How many Websites completed");continue;}while(isNaN(WebsitesFinishedTypeIn) == true);}
+if(WebsitesFinishedTypeIn >=100)
+{
+    alert("No way you could've finished so many");
+    console.log("No way you could've finished so many");
+    //Loops the prompt if the employee enters a letter or word until they input a number//
+    do{WebsitesFinishedTypeIn = prompt("How many Websites completed");continue;}while(isNaN(WebsitesFinishedTypeIn) == true);
     
-    }else{
+    //If the number entered in the prompt is below 8 then employee will receive an alert letting them know that sadly they will not receive a bonus//
+}else{
         alert("Sorry, no bonus for you!");
         console.log("Sorry, no bonus for you!");
 }
+
+//document.write("<br/>");
+//feedback to employee//
+if(WebsitesFinishedTypeIn >=8)
+   {
+    document.write("Good job! On to the next projects.");
+    }
+    
