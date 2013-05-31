35983e894c55d8ce76c882209dc7ba62e045f2e7
quinoriosJD7 <quino.rios.jd@hotmail.com>
2013-05-30 22:05:11 -0400

    Industry functions
    javascript modified

--- week4_functions/industrial_functions/javascript/industrial_functions.js
+++ week4_functions/industrial_functions/javascript/industrial_functions.js
@@ -1,19 +1,38 @@
 //Jorge Quinones Rios may 30, 2013 Industry functions
 
-//block of code to be repeated to obtain information from employee//
-function numberOfcssFiles(){
+
     //promt asking how many css files need to be created//
     var employeeInfo = prompt("Css files missing?");
+    
+    if(employeeInfo >=5){
+        alert("wrong");
+        console.log("wrong");
+      do{employeeInfo = prompt("Css files missing?");continue;}while(isNaN(employeeInfo) == true);}
+    
+
+//block of code to be repeated to obtain information from employee//
+function numberOfcssFiles(){
+    
+
+    
     //total of css files that need to be created//
     var cssFilesNeeded = 5;
+    
     //css files executed//
     var cssFilesCreated = 3;
+    
     //css files being attempted//
     var attemptingCSSfiles = 1;
+    
     //goal amount//
     var goal = cssFilesNeeded + attemptingCSSfiles;
+    
    //display of total//
     console.log("this is the total goal"+ ' ' + goal);}
+    
+    
+
+    
    numberOfcssFiles();
 
 
