7aa7a800fcf3864705d5c3217181b49773f6afe3
quinoriosJD7 <quino.rios.jd@hotmail.com>
2013-05-30 23:20:20 -0400

    Wacky functions
    javascript modified

---
+++ week4_functions/wacky_functions/javascript/wacky_functions.js
@@ -0,0 +1,14 @@
+var pairsOfTimbs = 5;
+var timbsPerweek = 5;
+var timbsOwned = prompt("How many pairs of Timbs do you own?");
+console.log("How many pairs of Timbs do you own?");
+if (timbsOwned >=6)
+{
+    alert("wrong")
+    console.log("wrong")
+}
+if (timbsOwned == 5)
+{
+    alert(correct);
+    console.log(correct);
+}
