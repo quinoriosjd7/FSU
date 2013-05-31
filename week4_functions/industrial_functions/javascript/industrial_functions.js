84fd8237d5a35aa4349605e4bd27aba0a9436a35
quinoriosJD7 <quino.rios.jd@hotmail.com>
2013-05-30 21:08:03 -0400

    Industry function
    first commit

---
+++ week4_functions/industrial_functions/javascript/industrial_functions.js
@@ -0,0 +1,13 @@
+function numberOfcssFiles(){
+    var cssFilesNeeded = 5;
+    var cssFilesCreated = 3;
+    var attemptingCSSfiles = 1;
+    var goal = cssFilesNeeded + attemptingCSSfiles;
+    var employeeInfo = prompt("Css files missing?");}
+    console.log(goal);
+numberOfcssFiles();
+
+if(employeeInfo == 5)
+    {alert("congrats")
+     console.log("congrats");
+     console.log("var goal");}
