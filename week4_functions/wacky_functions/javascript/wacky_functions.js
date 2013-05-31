ebd4d867083d9db030e7592b899eb8f998bfc037
quinoriosJD7 <quino.rios.jd@hotmail.com>
2013-05-30 23:41:21 -0400

    Wacky functions
    javascript file modified

--- week4_functions/wacky_functions/javascript/wacky_functions.js
+++ week4_functions/wacky_functions/javascript/wacky_functions.js
@@ -1,14 +1,34 @@
-var pairsOfTimbs = 5;
-var timbsPerweek = 5;
-var timbsOwned = prompt("How many pairs of Timbs do you own?");
-console.log("How many pairs of Timbs do you own?");
-if (timbsOwned >=6)
-{
-    alert("wrong")
-    console.log("wrong")
+//Jorge Quinones Rios may 30,2013 Wacky functions//
+
+
+//amount of times singers choke in a week//
+var singersChokeInaweek = 0;
+//amount of times singers choke in a month//
+var singersChokeInamonth = 1;
+//amount of times singers choke per concert//
+var chokesPerconcert = singersChokeInamonth * 5;
+//toe infections in a year
+var stressedVocalChords = singersChokeInamonth - 1;
+//promt asking user for stress days singers might go through//
+var singersStressLevel = prompt("How many stress days does a singer go through a year?");
+if(singersStressLevel >= 3 && singersStressLevel <20) {
+    alert("Yes!");
+    console.log("Yes!");
+    document.write("Correct");
+
+
+}//any other answer received as wrong//
+else{
+        alert("Wrong");
+        console.log("Wrong");
+        document.write("Incorrect");
+}{//repeats promt until user inputs a number//
+do{singersStressLevel = prompt("How many stress days does a singer go through a year?");continue;}while(isNaN(singersStressLevel) == true);}
+//block of code to be repeated//
+function stress(){
+    singersStressLevel * stressedVocalChords
+    alert("now thats stress");
+    console.log("now that's stress");
 }
-if (timbsOwned == 5)
-{
-    alert(correct);
-    console.log(correct);
-}
+
+stress();
