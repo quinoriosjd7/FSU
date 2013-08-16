5ec9dd3b91423de609a9a686a23577b8df048e4d
quinoriosJD7 <quino.rios.jd@hotmail.com>
2013-08-15 23:28:13 -0400

    Expressions_Industry
    First of three expressions assignments

---
+++ WPF_3_August/August_Expressions_Industry /javascript/JavaScript_1.js
@@ -0,0 +1,8 @@
+var websiteArtworks = 20;
+var designers = 10;
+var websites = 1;
+
+var designersPerArtwork = (websiteArtworks / designers)*2;
+alert("answer question");
+prompt("How many designers will work on the website's artwork? :")
+console.log(designersPerArtwork);
